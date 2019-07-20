7e94d5c55ea344b05c0321f529798d676811de37
Douglas <carvalhodouglas1999@gmail.com>
2019-07-20 16:47:36 -0300

    delete ok

---
+++ src/services/produtos.js
@@ -0,0 +1,18 @@
+import {http} from './config'
+
+export default{
+
+    listar:() => {
+        return http.get('produto')
+    },
+
+    salvar:(produto) =>{
+        return http.post('produto', produto)
+    },
+    atualizar:(produto) =>{
+        return http.put('produto', produto)
+    },
+    apagar:(produto) =>{
+        return http.delete('produto', {data:produto})
+    }
+}
